7158206df7aa3a601337e3b2d9a98f0b518d7578
DhananjayRathore0307 <85725378+DhananjayRathore0307@users.noreply.github.com>
2025-08-06 11:57:24 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://note-app-b12b.onrender.com/',
 });
 
 export default instance;
